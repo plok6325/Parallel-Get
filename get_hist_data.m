d1e78e7e926e0d22a57e38d15ca05608923a82ca
Hongbin <zhanghongbingod@outlook.com>
2016-08-29 15:35:12 +0800

    add get hist data weekly and monthly

--- get_hist_data.m
+++ get_hist_data.m
@@ -1,7 +1,13 @@
-function [ df ] = get_hist_data( code )
-%获取全部日线级别数据
+function [ df ] = get_hist_data( code,ktype)
+%获取全部历史数据
 symbol=code_to_symbol(code);
-page=urlread(['http://api.finance.ifeng.com/akdaily/?code=',symbol,'&type=last']);
+if ktype=='M'||ktype=='m'
+    page=urlread(['http://api.finance.ifeng.com/akmonthly/?code=',symbol,'&type=last']);
+elseif ktype=='W'||ktype=='w'
+   page=urlread(['http://api.finance.ifeng.com/akweekly/?code=',symbol,'&type=last']);
+elseif ktype=='D'||ktype=='d'
+    page=urlread(['http://api.finance.ifeng.com/akdaily/?code=',symbol,'&type=last']);
+end
 exp='(?<date>\d{4}\-\d{2}\-\d{2})\","(?<open>\d*\.\d*)\","(?<high>\d*\.\d*)\","(?<close>\d*\.\d*)\","(?<low>\d*\.\d*)\","(?<volumne>\d*\.\d*)\","(?<p_change>\S{5,6})\"';
 df=regexp(page,exp,'names');
 df=struct2table(df);
