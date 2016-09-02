156dd87e7f8d8af5fdeb560ccf95336706404c57
hongbin zhang <zhanghongbingod@outlook.com>
2016-09-03 00:57:46 +0800

    add week month and day

--- get_hist_data.m
+++ get_hist_data.m
@@ -4,13 +4,18 @@ symbol=code_to_symbol(code);
 if ktype=='M'||ktype=='m'
     page=urlread(['http://api.finance.ifeng.com/akmonthly/?code=',symbol,'&type=last']);
 elseif ktype=='W'||ktype=='w'
-   page=urlread(['http://api.finance.ifeng.com/akweekly/?code=',symbol,'&type=last']);
+    page=urlread(['http://api.finance.ifeng.com/akweekly/?code=',symbol,'&type=last']);
 elseif ktype=='D'||ktype=='d'
     page=urlread(['http://api.finance.ifeng.com/akdaily/?code=',symbol,'&type=last']);
+
 end
+ 
 exp='(?<date>\d{4}\-\d{2}\-\d{2})\","(?<open>\d*\.\d*)\","(?<high>\d*\.\d*)\","(?<close>\d*\.\d*)\","(?<low>\d*\.\d*)\","(?<volumne>\d*\.\d*)\","(?<p_change>\S{5,6})\"';
 df=regexp(page,exp,'names');
 df=struct2table(df);
 df=sortrows(df,'date','descend');
+% if 
+% else
+%     page=urlread(['http://api.finance.ifeng.com/akmin?scode=sz000002&type=',num2str(ktype)]);
 end
 
